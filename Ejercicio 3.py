3db63b352ae7e07d9c05a52c5a4287163b3a5b37
sebasquintero912 <sebastian.quinteroet32@gmail.com>
2025-07-14 11:06:10 -0300

    Create Ejercicio 3.py

---
+++ Ejercicio 3.py
@@ -0,0 +1,15 @@
+def columna_maximo_en_fila(matriz, fila):
+    maximo = matriz[fila][0]
+    indice = 0
+    for i in range(1, len(matriz[fila])):
+        if matriz[fila][i] > maximo:
+            maximo = matriz[fila][i]
+            indice = i
+    return indice
+
+matriz = [
+    [0, 4, 2, 1],
+    [5, 0, 3, 2],
+    [0, 2, 0, 1],
+    [1, 0, 2, 0]
+]
